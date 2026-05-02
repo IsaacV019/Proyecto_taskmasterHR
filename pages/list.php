4b66531ba9a0f0ee591f4a550f6133e8b88f6b21
Alex Javier Hernandez Rosales <alexjavier123hr@gmail.com>
2026-05-02 16:17:26 -0600

    cambios en list.php

--- pages/list.php
+++ pages/list.php
@@ -4,11 +4,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>TaskMaster — Lista de Tareas</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../css/bootstrap.min.css" rel="stylesheet">
+  <link href="font/bootstrap-icons.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+  <link href="../css/fuentes.css" rel="stylesheet">
   <link href="../css/style.css" rel="stylesheet">
- 
 </head>
 <body>
 
